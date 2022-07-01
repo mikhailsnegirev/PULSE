b3568f19b5161ca436e1c05cfbf60ed0af74707f
Mikhail <snegirev070502@mail.ru>
2022-07-01 17:49:30 +0300

    error addaptive carousel

--- src/js/script.js
+++ src/js/script.js
@@ -1,18 +1,35 @@
 $(document).ready(function(){
   $('.carousel__inner').slick({
       speed: 1200,
-      adaptiveHeight: true,
+      adaptiveHeight: false,
       prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></img></button>',
       nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></img></button>',
-      responsive: [
-        {
-          breakpoint: 992,
-          settings: {
-              dots: true,
-              arrows:false,
-          }
-        }
-      ]
+   responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
 });
 });
-		
